4abf4b44f86ecd4cf21191e3aa72c5a4e3309387
masashi-a-kurita <masashi.a.kurita@dena.com>
2015-09-10 19:52:57 +0900

    add webview and write IP address into log

--- app/src/main/java/com/example/masashiakurita/androidawstestsample/MainActivity.java
+++ app/src/main/java/com/example/masashiakurita/androidawstestsample/MainActivity.java
@@ -2,15 +2,29 @@ package com.example.masashiakurita.androidawstestsample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 public class MainActivity extends Activity {
+
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getLocalIpv4Address();
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("http://www.google.com");
     }
 
     @Override
@@ -33,5 +47,26 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void getLocalIpv4Address() {
+        try {
+            Enumeration<NetworkInterface> networkInterfaceEnum = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaceEnum.hasMoreElements()) {
+                NetworkInterface networkInterface = networkInterfaceEnum.nextElement();
+
+                Enumeration<InetAddress> ipAddressEnum = networkInterface.getInetAddresses();
+                while (ipAddressEnum.hasMoreElements()) {
+                    InetAddress inetAddress = (InetAddress) ipAddressEnum.nextElement();
+
+                    if (!inetAddress.isLoopbackAddress()) {
+                        Log.d(TAG, inetAddress.toString());
+                    }
+                }
+
+            }
+        } catch (SocketException e) {
+            Log.e(TAG, e.toString());
+        }
     }
 }
